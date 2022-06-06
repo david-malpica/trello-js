6faac4483dbf7e2dc7190c50c92d5d1a3609ef5e
David Malpica <dmalpica96@hotmail.com>
2022-06-06 16:40:57 -0500

    Add trello dep

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ require('dotenv').config()
 if(!process.env.TOKEN && !process.env.KEY){
   throw new Error('No hay configuración con Api Key y con Token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`

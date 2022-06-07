c639e4d6ba69456fe64322186a0ec185b240a725
David Malpica <dmalpica96@hotmail.com>
2022-06-07 16:43:32 -0500

    Reinstalling trello

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ let Trello = require("trello");
 let trello = new Trello(process.env.KEY, process.env.TOKEN);
 
 let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "6264e42be72d295e64f5c083",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	});

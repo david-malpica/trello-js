d5fbb090928dc8448e9fa6e58d45d6a948344e2a
David Malpica <dmalpica96@hotmail.com>
2022-06-10 14:03:11 -0500

    Add list in trello

--- app.js
+++ app.js
@@ -9,12 +9,22 @@ let trello = new Trello(process.env.KEY, process.env.TOKEN);
 
 let cardTitle = `Card Nueva ${new Date()}`
 
-trello.addCard(cardTitle, "LaunchX Card Description", "6264e42be72d295e64f5c083",
+trello.addCard(cardTitle, "LaunchX Card Description", "62a38fea2866035c938557f9",
 	function (error, trelloCard) {
 		if (error) {
 			console.log('Could not add card:', error);
 		}
 		else {
 			console.log('Added card:', trelloCard);
+		}
+	});
+
+trello.addListToBoard("62a38defa40d26675550b90c","API list test",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add list:', error);
+		}
+		else {
+			console.log('Added list', trelloCard);
 		}
 	});
